52b9be85287d4b4aa748ce6d8800c3dfec19e1de
isaac <isaacruz2003@gmail.com>
2024-12-09 00:11:21 +0100

    Finit

---
+++ src/main/java/org/iesvdm/jsp_servlet_jdbc/servlet/BorrarSociosServlet.java
@@ -0,0 +1,33 @@
+package org.iesvdm.jsp_servlet_jdbc.servlet;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.iesvdm.jsp_servlet_jdbc.dao.SocioDAO;
+import org.iesvdm.jsp_servlet_jdbc.dao.SocioDAOImpl;
+import org.iesvdm.jsp_servlet_jdbc.model.Socio;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "BorrarSociosServlet", value = "/BorrarSociosServlet")
+public class BorrarSociosServlet extends HttpServlet {
+    private SocioDAO socioDAO = new SocioDAOImpl();
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = null;
+        try {
+            int socioId = Integer.parseInt(request.getParameter("codigo"));
+            this.socioDAO.delete(socioId);
+
+            dispatcher = request.getRequestDispatcher("ListarSocioServlet");
+        }catch (NumberFormatException e){
+            request.setAttribute("error", "Error!");
+            dispatcher = request.getRequestDispatcher("ListarSocioServlet");
+        }
+        dispatcher.forward(request,response);
+    }
+}
